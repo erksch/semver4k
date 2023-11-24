16603d07c63186e04381ba36447dbe1fe4c12048
Leon Kiefer <leon.k97@gmx.de>
2023-11-24 16:24:35 +0100

    Set kotlin jvmToolchain in  convertion-plugins

--- convention-plugins/build.gradle.kts
+++ convention-plugins/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     `kotlin-dsl`
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 dependencies {
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
